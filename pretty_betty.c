646708a072105f11e569582ddfb228b542f0d9cc
ozzeal <ozygybaby1212@gmail.com>
2023-08-15 14:22:16 -0700

    proud betty

---
+++ pretty_betty.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - this adds two intergers
+ * Return: Always 0 success
+ */
+int main(void)
+{
+	int x = 20;
+	int y = 10;
+	int result;
+
+	result = x + y;
+	printf("Addition of x and y is:%d\n", result);
+	return (0);
+}
